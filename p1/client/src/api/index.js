50b966523d1e4583dec06ac4c8f7d6665d602e2b
samsawan <sam@skiplist.com>
2020-04-11 15:54:46 -0400

    start the consolidation of the api

---
+++ p1/client/src/api/index.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+export default API = {
+  addEvent: ({ data }) => {
+    axios({
+      data,
+      method: "POST",
+      url: "http://localhost:5000/auth/api/events",
+    })
+      // log response from server
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+  getEvents: ({ eventId, successfulCb }) => {
+    axios
+      // api route
+      .get("http://localhost:5000/auth/api/event/" + eventId)
+      .then(successfulCb)
+      .catch((error) => {
+        console.log(error.events);
+      });
+  },
+  getUsersAttendingEvents: ({ eventId, successfulCb }) => {
+    axios
+      .get("http://localhost:5000/auth/api/userAttendingEvents/" + eventId)
+      .then(successfulCb)
+      .catch((error) => {
+        console.log(error.events);
+      });
+  },
+};
