6c79f25f746d7050253ed8e504d42f995ce37cf6
youngmasterhiei <james.illius@yahoo.com>
2020-04-18 02:43:25 -0400

    cleaned it up a tad, changed a variables name, removed console log

--- p1/client/src/js/components/ProfileComps/ViewUserCard.js
+++ p1/client/src/js/components/ProfileComps/ViewUserCard.js
@@ -40,9 +40,7 @@ class UserInfoCard extends Component {
 
   renderUsersProfile = (userInfoType) => {
     if (userInfoType != null) {
-      console.log(userInfoType);
-
-      const MyComponent = (
+      const UsersDetails = (
         <ul style={{ listStyle: "none" }}>
           {Object.keys(userInfoType).map((key) => (
             <li key={key}>{userInfoType[key]}</li>
@@ -50,9 +48,10 @@ class UserInfoCard extends Component {
         </ul>
       );
 
-      return [MyComponent];
+      return [UsersDetails];
     } else {
       console.log("no data ran");
+      // ******saving this for later use******
       // const dropdown = (
       //   <DropDownForm
       //     name={formTitle}
@@ -60,7 +59,7 @@ class UserInfoCard extends Component {
       //     formApiAction={"post"}
       //   />
 
-      return <h4>no user data</h4>;
+      return <h4>Users profile incomplete</h4>;
     }
   };
 
@@ -77,7 +76,6 @@ class UserInfoCard extends Component {
     return (
       <div style={{ display: "flex" }}>
         <div>
-          {/* TODO: make local */}
           <img
             src="https://images.idgesg.net/images/article/2017/06/reactjs_code_coding_thinkstock-100725807-large.jpg"
             width="400"
