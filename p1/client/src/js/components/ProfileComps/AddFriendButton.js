9e4202c08932cfdb667a8a77b39fba311e92f698
youngmasterhiei <james.illius@yahoo.com>
2020-04-18 04:07:27 -0400

    the friend request now targets specific id of user

--- p1/client/src/js/components/ProfileComps/AddFriendButton.js
+++ p1/client/src/js/components/ProfileComps/AddFriendButton.js
@@ -4,12 +4,14 @@ import { API } from "../../../api";
 const AddFriendButton = (props) => {
   const friendSubmit = (e) => {
     console.log("hello");
+    console.log(props.data.id);
+
     const userId = localStorage.getItem("token");
     e.preventDefault();
     const notification = {
       messageType: e.target.name,
       fromUserId: userId,
-      receivingUserId: props.data[0].userId,
+      receivingUserId: props.data.id,
       message: "Requested you as a friend",
       userId: userId,
     };
